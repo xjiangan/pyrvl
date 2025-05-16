9b62123e34889f73719de5306ef3917035a6234c
Xudong Jiang <xudong.jiang@inf.ethz.ch>
2025-05-16 17:39:16 +0200

    test

---
+++ tests/test_basic.py
@@ -0,0 +1,11 @@
+from pyrvl import compress, decompress
+import numpy as np
+
+def test_compress():
+    for i in range(100):
+        width= np.random.randint(0, 8192)
+        height= np.random.randint(0, 8192)
+        input = np.random.randint(0, 65535, (height, width), dtype=np.uint16)
+        compressed = compress(input)
+        decompressed = decompress(compressed)
+        assert np.array_equal(input, decompressed)
