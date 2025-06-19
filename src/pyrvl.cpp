042aae437c19e00bc0434131da6aca944c951db6
Xudong Jiang <xudong.jiang@inf.ethz.ch>
2025-06-19 17:41:42 +0200

    license

--- src/pyrvl.cpp
+++ src/pyrvl.cpp
@@ -1,3 +1,29 @@
+// MIT License
+
+// Copyright (c) 2017 Andrew D. Wilson
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// Andrew D. Wilson. 2017. Fast Lossless Depth Image Compression. 
+// In Proceedings of the 2017 ACM International Conference on Interactive Surfaces and Spaces (ISS '17). 
+// Association for Computing Machinery, New York, NY, USA, 100–105. https://doi.org/10.1145/3132272.3134144
+
 #include <nanobind/nanobind.h>
 #include <nanobind/ndarray.h>
 namespace nb = nanobind;
