c8e15496a5d619bdb0bd542c0818bac194f7f788
Xudong Jiang <xudong.jiang@inf.ethz.ch>
2025-06-19 18:13:48 +0200

    less test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,7 +2,7 @@ from pyrvl import compress, decompress
 import numpy as np
 
 def test_compress():
-    for i in range(100):
+    for i in range(10):
         width= np.random.randint(0, 8192)
         height= np.random.randint(0, 8192)
         input = np.random.randint(0, 65535, (height, width), dtype=np.uint16)
